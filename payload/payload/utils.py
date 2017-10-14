9dacc65d8f7c5a6a499c796c5b475a9a0525e012
Dennis Lin <dennis.of.lin@gmail.com>
2017-10-14 16:12:29 -0400

    [Main] Adding main payload app

---
+++ payload/payload/utils.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_envvar(var_name, default=None, show_warnings=True):
+    """
+    Get the environment variable or return exception
+    """
+    value = default
+
+    try:
+        value = os.environ[var_name]
+    except KeyError:
+        value = None
+        
+    if value is None:
+        err = "Environment variables %s not set" % var_name
+        raise ImproperlyConfigured(err)
+
+    return value
+
