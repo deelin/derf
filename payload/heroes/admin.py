b0ba3eaa5f245bc6b46c0d9414d983bb7f6b4a1b
Dennis Lin <dennis.of.lin@gmail.com>
2017-10-29 22:40:37 -0400

    [Heroes] Add interaction to admin

--- payload/heroes/admin.py
+++ payload/heroes/admin.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Ability, Emote, Hero, Role
+from .models import Ability, Emote, Hero, Interaction, Role
 
 
 admin.site.register(Ability)
 admin.site.register(Emote)
 admin.site.register(Hero)
+admin.site.register(Interaction)
 admin.site.register(Role)
